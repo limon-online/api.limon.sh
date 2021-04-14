c41ef5f82ff2e4cb0a5f20c9a1335af73fcc1a44
Andrey Doroschenko <dorosch.github.io@yandex.ru>
2021-04-14 13:47:16 +0300

    Update CI/CD process

--- service/settings/base.py
+++ service/settings/base.py
@@ -68,11 +68,11 @@ CONN_MAX_AGE = 60
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT')
+        'NAME': os.environ['POSTGRES_DB'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '')
     }
 }
 
